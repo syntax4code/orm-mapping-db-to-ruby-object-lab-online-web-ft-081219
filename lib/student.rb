3c2e6238f81014fe717530bc5f52562f2d221c35
Warren Harper II <warrenharper2@att.net>
2019-10-01 20:13:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,16 @@ class Student
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
     DB[:conn].execute(sql, x)
   end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
+
+    def self.all_students_in_grade_X(x)
+      sql = "SELECT * FROM students WHERE grade = ?"
+      DB[:conn].execute(sql, x)
+    end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
