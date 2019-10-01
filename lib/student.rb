be15961b7c57600ce061c239cef3a34cf5d76948
Warren Harper II <warrenharper2@att.net>
2019-10-01 20:16:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    # 
+    #
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
     first_student_row = DB[:conn].execute(sql)[0]
     self.new_from_db(first_student_row)
